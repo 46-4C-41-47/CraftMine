3657abe407f1192b64800cc5521ec9abc7638910
46-4C-41-47 <Laptop@gmail.com>
2023-09-13 11:38:01 +0200

    azerty

--- CraftMine/main.cpp
+++ CraftMine/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include "include/Shader.h"
 
 int main() {
 	std::cout << "hello world" << std::endl;
+
+	Shader shader("./res/vertexShader.glsl", "./res/fragmentShader.glsl");
 
 	return 0;
 }
